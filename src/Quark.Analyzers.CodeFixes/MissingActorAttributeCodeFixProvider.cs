d4abea78b5ffcd9e33192d84bf51f44c765f0846
thanh <thanhhero765@gmail.com>
2026-02-02 08:34:58 +0700

    feat: Add ProcessComplexObjectAsync method and corresponding ObjectClassValue class for complex object handling

--- src/Quark.Analyzers.CodeFixes/MissingActorAttributeCodeFixProvider.cs
+++ src/Quark.Analyzers.CodeFixes/MissingActorAttributeCodeFixProvider.cs
@@ -1,15 +1,12 @@
 using System.Collections.Immutable;
 using System.Composition;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace Quark.Analyzers;
+namespace Quark.Analyzers.CodeFixes;
 
 /// <summary>
 /// Code fix provider for adding missing [Actor] attribute to actor classes.
